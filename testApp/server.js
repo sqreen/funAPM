dfbd40b98da2e8624be8c8e0c4b239ddef7e3e4d
vdeturckheim <vlad2t@hotmail.com>
2017-10-30 10:41:34 +0100

    bootstrap project and add test app

---
+++ testApp/server.js
@@ -0,0 +1,52 @@
+'use strict';
+const Express = require('express');
+const Mongoose = require('mongoose');
+const BodyParser = require('body-parser');
+
+const parseJSON = BodyParser.json();
+
+
+const app = Express();
+Mongoose.connect('mongodb://localhost/test', { useMongoClient: true });
+Mongoose.Promise = global.Promise;
+
+const Cat = Mongoose.model('Cat', { name: String });
+
+app.use(function(err, req, res, next) {
+
+    console.error(err);
+    return next(err);
+});
+
+app.post('/cats', parseJSON, (req, res, next) => {
+
+    if (typeof req.body.name !== 'string') { // TODO: use celbrate somedays
+        const err = new Error('wrong body format');
+        return setTimeout(() => next(err));
+    }
+    const kitty = new Cat({ name: req.body.name });
+    kitty.save(function (err) {
+        if (err) {
+            return next(err);
+        } else {
+            res.status(201);
+            return res.end('');
+        }
+    });
+});
+
+app.get('/cats', async (req, res, next) => {
+
+    try {
+        const cats = await Cat.find().exec();
+        return res.json(cats);
+    }
+    catch (e) {
+        return next(e);
+    }
+});
+
+app.listen(9090, () => {
+
+    console.log('server running on port 9090');
+});
