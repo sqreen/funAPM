f712932be1bca9b77e0d2723c3ccc387e4d09f2b
vladimir <vlad2t@hotmail.com>
2017-11-03 00:49:35 +0100

    fff

--- convert.js
+++ convert.js
@@ -1,27 +1,131 @@
 'use strict';
 const Profile = require('./profile.json'); // todo stream ?
 
+const nodesMap = new Map();
 
-const transform = function (node) {
+const parse = function (node, parentId) {
 
-    const res = {};
-    res.name = `${node.url}:${node.lineNumber} - ${node.functionName} - ` + Math.random();
-    res.children = node.children.map((x) => transform(x));
-    res.value = node.hitCount + res.children.map((x) => x.value).filter(Boolean).reduce((a, b) => a + b, 0);
-
-    if (node.functionName.includes('idle')) {
-        // console.log(node)
-        res.value = 1
+    if (parentId !== undefined) {
+        node.parentId = parentId;
     }
+    nodesMap.set(node.id, node);
 
-    if (isNaN(res.value) ){
-        console.log(node.hitCount)
-        console.log(res.children)
-        console.log(res.children.reduce((a, b) => a.value + b.value, 0))
-        throw ''
+    node.children.forEach((x) => {
+
+        parse(x, node.id);
+    });
+};
+
+parse(Profile.head);
+
+const getCallChain = function (nodeId) {
+
+    const node = nodesMap.get(nodeId);
+
+    const res = [node.id];
+    if (node.parentId !== undefined) {
+        return getCallChain(node.parentId).concat(res);
     }
-
     return res;
 };
-// transform(Profile.head)
-console.log(JSON.stringify(transform(Profile.head)));
+
+const getName = (node) => `${node.functionName}` || `(${node.url}:${node.lineNumber})`;
+
+const builtChain = Profile.samples.map((x) => getCallChain(x));
+
+const reverseAndNormalize = function (table) {
+
+    const ln = Math.max(...table.map((x) => x.length));
+    const res = [];
+    for (let i = 0; i < table.length; ++i) {
+        for (let j = 0; j < ln; ++j) {
+
+            if (res[j] === undefined) {
+                res[j] = [];
+            }
+            res[j][i] = table[i][j] || -1;
+        }
+    }
+    return res;
+};
+
+const chain  = reverseAndNormalize(builtChain);
+
+const merge = function (ch) {
+
+    let current = null;
+    const result = [];
+    for (let i = 0; i < ch.length; ++i) {
+        result[i] = [];
+        current = null;
+        for (let j = 0; j < ch[i].length; ++j) {
+
+            if (current === null) {
+                current = { id: ch[i][j], count: 1 };
+                continue;
+            }
+            if (current.id === ch[i][j]){
+                current.count++;
+                continue;
+            }
+            result[i].push(current);
+            current = { id: ch[i][j], count: 1 };
+        }
+        result[i].push(current);
+    }
+    return result;
+};
+
+const merged = merge(chain);
+
+// console.log(merged.map((x) => JSON.stringify(x)).join('\n'));
+
+const build = function (mergedChain) {
+
+    for (let i = 1; i < mergedChain.length; ++i) {
+        let rankInPreviousLine = 0;
+        const previousLine = mergedChain[i - 1];
+        const line = mergedChain[i];
+        for (let j = 0; j < line.length; ++j) {
+
+            if (line[j].id === -1) {
+                continue;
+            }
+            const node = nodesMap.get(line[j].id);
+            while (node.parentId !== previousLine[rankInPreviousLine].id) {
+                rankInPreviousLine++;
+            }
+            previousLine[rankInPreviousLine].children = previousLine[rankInPreviousLine].children || [];
+            previousLine[rankInPreviousLine].children.push(line[j]);
+        }
+    }
+};
+build(merged);
+const root = merged[0][0];
+
+const run = function (item) {
+
+    const node = nodesMap.get(item.id);
+    const res = {
+        name: getName(node),
+        value: item.count,
+        id: item.id,
+        children: (item.children || []).map((x) => run(x))
+    };
+    if (res.children.length > 0) {
+        res.count = res.children.filter(Boolean).map((x) => x.count).filter(Boolean).reduce((a, b) => a + b, 0);
+    }
+    return res;
+};
+
+const finam = run(root);
+
+finam.children.forEach((item) => {
+
+    if (item.name.includes('idle')) {
+        finam.value = finam.value - item.value + 10;
+        item.value = 10;
+    }
+});
+
+console.log(JSON.stringify(finam, null, 2))
